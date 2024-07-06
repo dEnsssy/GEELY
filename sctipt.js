d001cef7c02d78d89b89bd2534acc698e301b41c
dEnsssy <kravchuk.n.o@gmail.com>
2024-07-06 14:53:56 +0300

    v.1.0 with all functional,no Adaptive

--- sctipt.js
+++ sctipt.js
@@ -3,22 +3,21 @@ const btnAddCars = document.getElementById("moreCars");
 let i = 0;
 function addCar() {
     const newCard = card.cloneNode(true);
-    newCard.id = `car${i}`;
+    let idCard = `car${i}`;
+    newCard.id = idCard;
     i++;
+    const fancyboxLinks = newCard.querySelectorAll(
+        ".main__cards-card-mini a[data-fancybox]"
+    );
+    fancyboxLinks.forEach((link) => {
+        link.setAttribute("data-fancybox", idCard);
+    });
     let listCar = document.querySelector(".main__cards");
     listCar.appendChild(newCard);
+    console.log(newCard);
 }
 btnAddCars.addEventListener("click", () => {
     addCar();
     addCar();
     addCar();
 });
-
-// document.querySelectorAll("a").addEventListener(
-//     "click",
-//     function (event) {
-//         event.preventDefault();
-//         return false;
-//     },
-//     false
-// );
